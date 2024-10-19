31496e91889e653155f8337df2e9fea7fb854067
Noble <nortexnoble@gmail.com>
2024-10-19 13:33:31 +0000

    fixed commiting

--- ex_maintenance/ex_maintenance/doctype/ex_requests/ex_requests.py
+++ ex_maintenance/ex_maintenance/doctype/ex_requests/ex_requests.py
@@ -1,68 +1,19 @@
 # # Copyright (c) 2024, Nortex and contributors
 # # For license information, please see license.txt
 
-# import frappe
-# from frappe.model.document import Document
-
-
-# class ExRequests(Document):
-# 	pass
-
-
-# # ! Function to create new work order doc for manager 
-# @frappe.whitelist()
-# def create_work_order(event):
-#     try: 
-#         # ? This is to get the document fields
-#         work_order = frappe.get_doc('Ex Requests', event)
-        
-#         # ? Getting and assigning the fields to each other
-#         get_fields = frappe.get_doc({
-#             'doctype': 'Ex Work Order',
-#             'name1': event.location,
-#             'room_number': event.room_number,
-#             'other': event.other,
-#             'request_code': event.id,
-#             'date': event.date,
-#             'time': event.time,
-#             'further_information': event.further_description,
-#             'issue': []
-#         })
-        
-#         # ? For loop to get table contents
-#         for item in event.table_umyd:
-#             get_fields.append('issue', {
-#                 'issue_type': item.issue_type,
-#                 'description': item.description,
-#             })		
-
-
-#         get_fields.insert()
-#         get_fields.submit()
-#         work_order.save()
-
-#         return get_fields.name
-
-#     except Exception as e:
-#         frappe.log_error(f"Error creating Work Order: {e}")
-#         return str(e)
-
-
-
 import frappe
 from frappe.model.document import Document
 
 class ExRequests(Document):
-    # Hook into the document save event (this runs after the document is inserted)
     def after_insert(self):
-        # Call the method to create the Work Order
+        # ? Call the method to create the Work Order
         create_work_order(self)
 
 # ! Function to create new work order doc for manager 
 @frappe.whitelist()
 def create_work_order(ex_request):
     try:
-        # Create the new Work Order document
+        # ? Create the new Work Order document
         work_order = frappe.get_doc({
             'doctype': 'Ex Work Order',
             'name1': ex_request.location,  # Assuming location is a field in Ex Request
@@ -75,7 +26,7 @@ def create_work_order(ex_request):
             'issue': []  # Empty table, we will populate it below
         })
 
-        # ?Iterate over the child table (table_umyd) in Ex Requests, if it exists
+        # ? Iterate over the child table (table_umyd) in Ex Requests, if it exists
         if ex_request.table_umyd:
             for item in ex_request.table_umyd:
                 # Append each issue in the child table to the Work Order's issue field
